894f3f3b3f01d23c5ad78bac42cd3895379f91e1
Tom Hummel <tohu@hey.com>
2021-10-10 11:07:32 -0700

    add event send to honeycomb for tomhummel.com requests

--- workers/index.js
+++ workers/index.js
@@ -1,10 +1,11 @@
 // from: https://developers.cloudflare.com/workers/templates/pages/alter_headers/
 
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+  event.respondWith(handleRequest(event))
 })
 
-async function handleRequest(request) {
+async function handleRequest(event) {
+  const {request} = event
   // pass through the original request
   let response = await fetch(request.url, request)
 
@@ -16,5 +17,37 @@ async function handleRequest(request) {
 
   // https://infosec.mozilla.org/guidelines/web_security#x-xss-protection
   response.headers.set('X-XSS-Protection', '1; mode=block')
+
+  const { pathname } = new URL(event.request.url)
+  const cf = event.request.cf !== undefined ? event.request.cf : {}
+  const headers = new Map(event.request.headers)
+
+  let eventData = {
+    req_method: event.request.method,
+    req_pathname: pathname,
+    req_lat: cf.latitude,
+    req_lon: cf.longitude,
+    req_continent: cf.continent,
+    req_country: cf.country,
+    req_region: cf.region,
+    req_city: cf.city,
+    req_timezone: cf.timezone,
+    req_region_code: cf.regionCode,
+    req_metro_code: cf.metroCode,
+    req_postal_code: cf.postalCode,
+    req_colo: cf.colo,
+    req_cf_ray: headers.get('cf-ray')
+  }
+
+  event.waitUntil(postLog(eventData))
+
   return response
+}
+
+function postLog (data) {
+  return fetch('https://api.honeycomb.io/1/events/' + encodeURIComponent(HONEYCOMB_DATASET), {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: new Headers([['X-Honeycomb-Team', HONEYCOMB_KEY]]) 
+  })
 }
